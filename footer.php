3f0f04dfcb95c6ef1aa8b1a9953d981ff4f1c525
com05 <mousike@urok.co.kr>
2014-06-10 16:11:17 +0900

    init

---
+++ footer.php
@@ -0,0 +1,46 @@
+				<?php hybrid_get_sidebar( 'primary' ); // Loads the sidebar/primary.php template. ?>
+
+			</div><!-- #main -->
+
+			<?php hybrid_get_sidebar( 'subsidiary' ); // Loads the sidebar/subsidiary.php template. ?>
+
+		</div><!-- .wrap -->
+
+		<footer <?php hybrid_attr( 'footer' ); ?>>
+
+			<div class="wrap">
+
+
+<div id="footer">
+			<p class="logo"><img src="/images/common/footer_logo.gif" alt="기상청"></p>
+			<div class="footer_right">
+				<h3 class="blind">사이트 도우미</h3>
+				<ul class="site_helper">
+					<li><a href="http://web.kma.go.kr/global/footer/privacy.jsp"><img src="/images/common/footer_link_1.gif" alt="개인정보 처리방침"></a></li>
+					<li><a href="http://web.kma.go.kr/global/footer/siteinfo.jsp"><img src="/images/common/footer_link_2.gif" alt="이용안내"></a></li>
+					<li><a href="http://web.kma.go.kr/global/footer/copyright.jsp"><img src="/images/common/footer_link_3.gif" alt="저작권보호 및 정책"></a></li>
+					<li><a href="http://web.kma.go.kr/global/footer/accessibility.jsp"><img src="/images/common/footer_link_4.gif" alt="웹접근성정책"></a></li>
+					<li><a href="http://web.kma.go.kr/global/footer/return.jsp"><img src="/images/common/footer_link_5.gif" alt="홈페이지오류·건의"></a></li>
+					<li><a href="http://web.kma.go.kr/global/footer/telephone_1.jsp"><img src="/images/common/footer_link_6.gif" alt="전화번호안내"></a></li>
+					<li><a href="http://web.kma.go.kr/aboutkma/organization/personnel_list.jsp?schParent=1360001"><img src="/images/common/footer_link_7.gif" alt="부서·직원찾기"></a></li>
+					<li><a href="http://web.kma.go.kr/global/footer/viewer.jsp"><img src="/images/common/footer_link_8.gif" alt="뷰어다운로드"></a></li>
+				</ul>
+				<h3 class="blind">연락처 및 저작권</h3>
+				<address>
+					<p><img src="/images/common/footer_copyright_4.gif" alt="우. 156-720 서울시 동작구 여의대방로16길 61  대표전화 (02)2181-0900 (평일 9:00~18:00, 야간휴일은 당직실 연결)"> </p>
+					<p><img src="/images/common/footer_copyright_2.gif" alt="본 홈페이지에 게시된 이메일 주소가 자동 수집되는 것을 거부하며, 이를 위반시 정보통신망법에 의해 처벌됨을 유념하시기 바랍니다."></p>
+				</address>
+				<p class="copyright"><img src="/images/common/footer_copyright_3.gif" alt="Copyright © 2009 KMA. All Rights RESERVED. E-mail: webmasterkma@kma.go.kr"></p>
+				<p class="call_center"><a href="/aboutkma/intro/callcenter_greetion.jsp"><img src="/images/common/footer_call131.gif" alt="기상콜센터 전국 국번없이 131"></a></p>
+			</div>
+		</div>
+			</div><!-- .wrap -->
+
+		</footer><!-- #footer -->
+
+	</div><!-- #container -->
+
+	<?php wp_footer(); // WordPress hook for loading JavaScript, toolbar, and other things in the footer. ?>
+
+</body>
+</html>
